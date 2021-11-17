8931a996c252f9c20e7da58e82b39f7fef2823e2
imnaeem <ceinfo.pk@gmail.com>
2021-11-18 00:17:17 +0500

    ListView working

--- app/src/main/java/com/example/dialougeboxandlistviewtest/MainActivity.java
+++ app/src/main/java/com/example/dialougeboxandlistviewtest/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.dialougeboxandlistviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    ArrayList<String> frindlist = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = findViewById(R.id.myList);
+
+        frindlist.add("Naeem");
+        frindlist.add("Hammad");
+        frindlist.add("Ali");
+        frindlist.add("Sir");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, frindlist);
+        list.setAdapter(arrayAdapter);
+
     }
 }
