48ebe0cfcdd6ef514d9b93a7b9030481dc6122e3
imnaeem <ceinfo.pk@gmail.com>
2021-11-18 00:30:21 +0500

    ListView Add item working

--- app/src/main/java/com/example/dialougeboxandlistviewtest/MainActivity.java
+++ app/src/main/java/com/example/dialougeboxandlistviewtest/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.dialougeboxandlistviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button addBtn;
+    EditText fName;
     ListView list;
     ArrayList<String> frindlist = new ArrayList<String>();
 
@@ -19,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         list = findViewById(R.id.myList);
+        addBtn = findViewById(R.id.addbutton);
+        fName = findViewById(R.id.friendName);
 
         frindlist.add("Naeem");
         frindlist.add("Hammad");
@@ -27,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, frindlist);
         list.setAdapter(arrayAdapter);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                frindlist.add(fName.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
 
     }
 }
